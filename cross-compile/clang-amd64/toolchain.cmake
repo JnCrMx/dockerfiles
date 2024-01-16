ff0a4b9d24f5aceb940d793307fe964bf2feb497
JCM <jcm@jcm.re>
2024-01-16 10:38:36 +0000

    Improve toolchain.cmake

--- cross-compile/clang-amd64/toolchain.cmake
+++ cross-compile/clang-amd64/toolchain.cmake
@@ -1,8 +1,11 @@
+# System config
 SET(CMAKE_SYSTEM_NAME Linux)
 SET(CMAKE_SYSTEM_PROCESSOR amd64)
 
+# Prevent compiler test from failing due to linker (not sure if we still need this tbh)
 SET(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
 
+# Compiler config
 SET(CLANG_TARGET_TRIPLE x86_64-linux-gnu)
 
 SET(CMAKE_C_COMPILER clang)
@@ -10,4 +13,8 @@ SET(CMAKE_C_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
 SET(CMAKE_CXX_COMPILER clang++)
 SET(CMAKE_CXX_COMPILER_TARGET ${CLANG_TARGET_TRIPLE})
 
+# Package config
 SET(CMAKE_LIBRARY_ARCHITECTURE x86_64-linux-gnu)
+
+# Some misc stuff
+SET(CPACK_DEBIAN_PACKAGE_ARCHITECTURE amd64)
