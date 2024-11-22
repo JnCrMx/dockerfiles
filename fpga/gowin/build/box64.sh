4ec363b382d22d4a55ab5322be439376bbdbbd6e
JCM <jcm@jcm.re>
2024-11-22 03:15:52 +0100

    Add Dockerfile for Gowin EDA

---
+++ fpga/gowin/build/box64.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -p "$OUT"/
+
+if ! [ "$TARGETARCH" = "amd64" ] ; then
+    apt-get update
+    DEBIAN_FRONTEND=noninteractive apt-get install -y box64
+
+    for b in $BINS ; do
+        name=$(basename $b)
+        outpath="$OUT"/"$name"
+
+        echo "#!/bin/bash" > "$outpath"
+        echo "box64 \"$b\" \"\$@\"" >> "$outpath"
+        chmod +x "$outpath"
+    done
+else
+    for b in $BINS ; do
+        ln -s "$b" "$OUT"/
+    done
+fi
